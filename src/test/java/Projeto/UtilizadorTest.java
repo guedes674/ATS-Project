3d51b0b6bb4de999edd057d79a28867e007464cb
guedes674 <a97369@alunos.uminho.pt>
2025-04-09 20:30:34 +0100

    done some changes, changed the project and started some unit tests for user, next step, do one or two more classes of tests and finish them

---
+++ src/test/java/Projeto/UtilizadorTest.java
@@ -0,0 +1,49 @@
+package Projeto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+public class UtilizadorTest {
+    @Test
+    public void testUtilizadorVazio() {
+        // User object creation
+        UtilizadorPraticanteOcasional utilizador = new UtilizadorPraticanteOcasional();
+        utilizador.setNome("João");
+        utilizador.setMorada("Rua A");
+        utilizador.setEmail("joao@gmail.com");
+        utilizador.setFreqCardiaca(80);
+        utilizador.setPeso(75);
+        utilizador.setAltura(180);
+        utilizador.setDataNascimento(LocalDate.of(1990, 1, 1));
+        utilizador.setGenero('M');
+
+        // Assertions
+        assertEquals("João", utilizador.getNome());
+        assertEquals("Rua A", utilizador.getMorada());
+        assertEquals("joao@gmail.com", utilizador.getEmail());
+        assertEquals(80, utilizador.getFreqCardiaca());
+        assertEquals(75, utilizador.getPeso());
+        assertEquals(180, utilizador.getAltura());
+        assertEquals(LocalDate.of(1990, 1, 1), utilizador.getDataNascimento());
+        assertEquals('M', utilizador.getGenero());
+    }
+
+    @Test
+    public void testUtilizadorParametrizado() {
+        //User object creation
+        UtilizadorPraticanteOcasional utilizador = new UtilizadorPraticanteOcasional("Tiago", "Rua B", "tiago@gmail.com", 100, 70, 195,LocalDate.of(2002, 12, 4) , 'M');
+
+        // Assertions
+        assertEquals("Tiago", utilizador.getNome());
+        assertEquals("Rua B", utilizador.getMorada());
+        assertEquals("tiago@gmail.com", utilizador.getEmail());
+        assertEquals(100, utilizador.getFreqCardiaca());
+        assertEquals(70, utilizador.getPeso());
+        assertEquals(195, utilizador.getAltura());
+        assertEquals(LocalDate.of(2002, 12, 4), utilizador.getDataNascimento());
+        assertEquals('M', utilizador.getGenero());
+    }
+}
